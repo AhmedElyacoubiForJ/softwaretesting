18d1bcd1cfe6db13127c20507efc243a5243bdc9
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-16 00:41:21 +0200

    software testing project setup

---
+++ src/main/java/edu/yacoubi/softwaretesting/Main.java
@@ -0,0 +1,11 @@
+package edu.yacoubi.softwaretesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
