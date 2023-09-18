3fff3d234bddc708edc8f177f03e2c992b3cffaf
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-18 22:34:58 +0200

    setup before implementing the test using mock as repository

--- src/test/java/edu/yacoubi/softwaretesting/customer/CustomerRegistrationServiceTest.java
+++ src/test/java/edu/yacoubi/softwaretesting/customer/CustomerRegistrationServiceTest.java
@@ -1,15 +1,22 @@
 package edu.yacoubi.softwaretesting.customer;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class CustomerRegistrationServiceTest {
+    // test the class in isolation using a Mock for dependencies
+    @Mock
+    private CustomerRepository customerRepository;//  = mock(CustomerRepository.class);
 
     private CustomerRegistrationService underTest;
 
     @BeforeEach
     void setUp() {
-        //underTest = new CustomerRegistrationService();
+        MockitoAnnotations.initMocks(this);
+        underTest = new CustomerRegistrationService(customerRepository);
     }
 }
