817cfe8bac071d9466c666a364de8dd0c03d28b8
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-18 20:28:11 +0200

    test repo

---
+++ src/test/java/edu/yacoubi/softwaretesting/customer/CustomerRegistrationServiceTest.java
@@ -0,0 +1,15 @@
+package edu.yacoubi.softwaretesting.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerRegistrationServiceTest {
+
+    private CustomerRegistrationService underTest;
+
+    @BeforeEach
+    void setUp() {
+        //underTest = new CustomerRegistrationService();
+    }
+}
--- src/test/java/edu/yacoubi/softwaretesting/customer/CustomerRepositoryTest.java
+++ src/test/java/edu/yacoubi/softwaretesting/customer/CustomerRepositoryTest.java
@@ -3,13 +3,19 @@ package edu.yacoubi.softwaretesting.customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@DataJpaTest
+@DataJpaTest(
+        properties = {
+                "spring.jpa.properties.javax.persistence.validation.mode=none"
+        }
+)
 class CustomerRepositoryTest {
 
     @Autowired
@@ -18,8 +24,48 @@ class CustomerRepositoryTest {
     @Test
     void itShouldSelectCustomerByPhoneNumber() {
         // Given
+        UUID id = UUID.randomUUID();
+        String phoneNumber = "0000";
+        Customer customer = new Customer(id,"joe", phoneNumber);
+
+        // When
+        underTest.save(customer);
+
+        // Then
+        Optional<Customer> optionalCustomer = underTest
+                .selectCustomerByPhoneNumber(phoneNumber);
+        assertThat(optionalCustomer)
+                .isPresent()
+                .hasValueSatisfying(c -> {
+                    assertThat(c).isEqualToComparingFieldByField(customer);
+                });
+    }
+
+    @Test
+    void itNotShouldSelectCustomerByPhoneNumberWhenNumberDoesNotExists() {
+        // Given
+        String phoneNumber = "0000";
+
+        // When
+        Optional<Customer> optionalCustomer =
+                underTest.selectCustomerByPhoneNumber(phoneNumber);
+
+        // Then
+        assertThat(optionalCustomer).isNotPresent();
+    }
+
+    @Test
+    void itShouldNotSaveCustomerWhenNameIsNull() {
+        // Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, null, "0000");
+
         // When
         // Then
+        assertThatThrownBy(() -> underTest.save(customer))
+                //.hasMessageContaining("not-null property references a null or transient value : edu.yacoubi.sotwaretesting.customer.Customer.name")
+                .isInstanceOf(DataIntegrityViolationException.class);
+
     }
 
     @Test
@@ -42,5 +88,19 @@ class CustomerRepositoryTest {
 //                    assertThat(c.getPhoneNumber()).isEqualTo("0000");
                     assertThat(c).isEqualToComparingFieldByField(customer);
                 });
+    }
+
+    @Test
+    void itShouldNotSaveCustomerWhenPhoneNumberIsNull() {
+        // Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Alex", null);
+
+        // When
+        // Then
+        assertThatThrownBy(() -> underTest.save(customer))
+                .hasMessageContaining("not-null property references a null or transient value : edu.yacoubi.sotwaretesting.customer.Customer.phoneNumber")
+                .isInstanceOf(DataIntegrityViolationException.class);
+
     }
 }
