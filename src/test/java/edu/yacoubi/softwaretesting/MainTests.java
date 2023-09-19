44e0052a1992b79bbc3ea2cb61f99bfd6c17c49b
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-19 23:22:20 +0200

    added a method how to test as template

--- src/test/java/edu/yacoubi/softwaretesting/MainTests.java
+++ src/test/java/edu/yacoubi/softwaretesting/MainTests.java
@@ -3,11 +3,34 @@ package edu.yacoubi.softwaretesting;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class MainTests {
+
+	private Calculator underTest = new Calculator();
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void itShouldAddTwoNumbers() {
+		// Given
+		int a = 20;
+		int b = 30;
+
+		// When
+		int result = underTest.add(a, b);
+
+		// Then
+		int expected = 50;
+		assertThat(result).isEqualTo(expected);
+	}
+
+	class Calculator {
+		int add(int a, int b) {
+			return a + b;
+		}
+	}
 }
