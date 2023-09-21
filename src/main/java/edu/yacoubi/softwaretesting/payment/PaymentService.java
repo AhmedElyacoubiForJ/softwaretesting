627b06ad88720afa7cf37e347744173b61677626
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-21 21:37:41 +0200

    added a payment service w. customer & payment repositories as injection

---
+++ src/main/java/edu/yacoubi/softwaretesting/payment/PaymentService.java
@@ -0,0 +1,25 @@
+package edu.yacoubi.softwaretesting.payment;
+
+import edu.yacoubi.softwaretesting.customer.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class PaymentService {
+
+    private final CustomerRepository customerRepository;
+    private final PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentService(CustomerRepository customerRepository,
+            PaymentRepository paymentRepository) {
+        this.customerRepository = customerRepository;
+        this.paymentRepository = paymentRepository;
+    }
+
+    void chargeCard(UUID customerId, PaymentRequest request) {
+
+    }
+}
