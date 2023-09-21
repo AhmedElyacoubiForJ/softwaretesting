d34fbb2a431142c5414ef23fb566eb2da89e5ce4
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-21 21:27:28 +0200

    added a class to represent a request comming from a http client

---
+++ src/main/java/edu/yacoubi/softwaretesting/payment/PaymentRequest.java
@@ -0,0 +1,22 @@
+package edu.yacoubi.softwaretesting.payment;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PaymentRequest {
+    private Payment payment;
+
+    public PaymentRequest(@JsonProperty("payment") Payment payment) {
+        this.payment = payment;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    @Override
+    public String toString() {
+        return "PaymentRequest{" +
+                "payment=" + payment +
+                '}';
+    }
+}
