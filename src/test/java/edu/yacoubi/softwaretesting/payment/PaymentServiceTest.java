16e0fa8b1579d5aeb25e0a66c90f7720087b507e
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-22 21:04:39 +0200

    added a test routine Charge Card Happy Path

--- src/test/java/edu/yacoubi/softwaretesting/payment/PaymentServiceTest.java
+++ src/test/java/edu/yacoubi/softwaretesting/payment/PaymentServiceTest.java
@@ -1,4 +1,98 @@
-import static org.junit.jupiter.api.Assertions.*;
+package edu.yacoubi.softwaretesting.payment;
+
+import edu.yacoubi.softwaretesting.customer.Customer;
+import edu.yacoubi.softwaretesting.customer.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class PaymentServiceTest {
-  
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @Mock
+    private PaymentRepository paymentRepository;
+
+    @Mock // is a interface & we will test it in isolation later
+    private CardPaymentCharger cardPaymentCharger;
+
+    private PaymentService underTest;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        underTest = new PaymentService(
+                customerRepository,
+                paymentRepository,
+                cardPaymentCharger
+        );
+    }
+
+    @Test // Testing Charge Card Happy Path
+    void itShouldChargeCardSuccessfully() {
+        // Given
+        // we need a valid customer
+        UUID customerId = UUID.randomUUID();
+        // ... customer exists
+        given(customerRepository.findById(customerId))
+                // don't need a customer, so we mock it.
+                .willReturn(Optional.of(mock(Customer.class)));
+        // we know now that the first scenario
+        // according to the payment service implementation will pass
+
+        // ... payment request
+        PaymentRequest request = new PaymentRequest(
+                new Payment(
+                        null,
+                        null,
+                        new BigDecimal("100.00"),
+                        Currency.USD,
+                        "card123xx",
+                        "Donation"
+                )
+        );
+
+        // ... card is charged successfully
+        given(cardPaymentCharger.chargeCard(
+                request.getPayment().getSource(),
+                request.getPayment().getAmount(),
+                request.getPayment().getCurrency(),
+                request.getPayment().getDescription()
+        )).willReturn(new CardPaymentCharge(true));
+        // at this point every thing is fine
+
+        // When we charge a card ...
+        underTest.chargeCard(customerId, request);
+
+        // Then ... what can happen, we set the customer to the payment
+        // And we save the payment
+        // So the test must capture the payment and make sure that
+        // the customer set to it
+        ArgumentCaptor<Payment> paymentArgumentCaptor = ArgumentCaptor
+                .forClass(Payment.class);
+        then(paymentRepository)
+                .should()
+                .save(paymentArgumentCaptor.capture());
+        Payment paymentArgumentCaptorValue = paymentArgumentCaptor.getValue();
+        assertThat(paymentArgumentCaptorValue)
+                .isEqualToIgnoringGivenFields(
+                        request.getPayment(),
+                        "customerId"
+                );
+
+        assertThat(paymentArgumentCaptorValue.getCustomerId()).isEqualTo(customerId);
+    }
 }
