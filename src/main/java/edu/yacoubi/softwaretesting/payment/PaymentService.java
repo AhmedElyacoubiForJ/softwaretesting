2fe5ab117ee76694a69b7e0ca1ab0c90577a3e2f
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-22 12:29:13 +0200

    code format only

--- src/main/java/edu/yacoubi/softwaretesting/payment/PaymentService.java
+++ src/main/java/edu/yacoubi/softwaretesting/payment/PaymentService.java
@@ -1,12 +1,10 @@
 package edu.yacoubi.softwaretesting.payment;
 
-import edu.yacoubi.softwaretesting.customer.Customer;
 import edu.yacoubi.softwaretesting.customer.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -17,12 +15,16 @@ public class PaymentService {
     private final CardPaymentCharger cardPaymentCharger;
 
     private final List<Currency> ACCEPTED_CURRENCIES =
-            List.of(Currency.USD, Currency.GBP);
+            List.of(
+                    Currency.USD,
+                    Currency.GBP
+            );
 
     @Autowired
-    public PaymentService(CustomerRepository customerRepository,
-                          PaymentRepository paymentRepository,
-                          CardPaymentCharger cardPaymentCharger) {
+    public PaymentService(
+            CustomerRepository customerRepository,
+            PaymentRepository paymentRepository,
+            CardPaymentCharger cardPaymentCharger) {
         this.customerRepository = customerRepository;
         this.paymentRepository = paymentRepository;
         this.cardPaymentCharger = cardPaymentCharger;
@@ -32,35 +34,47 @@ public class PaymentService {
         Payment payment = request.getPayment();
 
         // 1. Does customer exist if not throw
-        boolean isCustomerFound = customerRepository.findById(customerId).isPresent();
+        boolean isCustomerFound = customerRepository
+                .findById(customerId)
+                .isPresent();
 
         if (!isCustomerFound) {
-            String message = String.format("customer [%s] not found", customerId);
+            String message = String.format(
+                    "customer [%s] not found",
+                    customerId
+            );
             throw new IllegalStateException(message);
         }
 
         // 2. Do we support currency if not throw
-        //boolean isCurrencySupported = ACCEPTED_CURRENCIES.contains(currency);
+        // ACCEPTED_CURRENCIES.contains(payment.getCurrency());
         boolean isCurrencySupported = ACCEPTED_CURRENCIES
                 .stream()
                 .anyMatch(c -> c.equals(payment.getCurrency()));
 
         if (!isCurrencySupported) {
-            String message = String.format("currency [%s] not supported", payment.getCurrency());
+            String message = String.format(
+                    "currency [%s] not supported",
+                    payment.getCurrency()
+            );
             throw new IllegalStateException(message);
         }
 
         // 3. Charge card
-        CardPaymentCharge cardPaymentCharge = cardPaymentCharger.chargeCard(
-                payment.getSource(),
-                payment.getAmount(),
-                payment.getCurrency(),
-                payment.getDescription()
-        );
+        CardPaymentCharge cardPaymentCharge = cardPaymentCharger
+                .chargeCard(
+                        payment.getSource(),
+                        payment.getAmount(),
+                        payment.getCurrency(),
+                        payment.getDescription()
+                );
 
         // 4. if not debited throw
         if(!cardPaymentCharge.isCardDebited()) {
-            String message = String.format("Card not debited for customer [%s]", customerId);
+            String message = String.format(
+                    "Card not debited for customer [%s]",
+                    customerId
+            );
             throw new IllegalStateException(message);
         }
 
