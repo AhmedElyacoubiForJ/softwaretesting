c6d20be9454ec40e7f72ce7ac92f869264d63474
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-23 00:17:01 +0200

    added stripe service implementation according to stripe api reference documentation

---
+++ src/main/java/edu/yacoubi/softwaretesting/stripe/StripeService.java
@@ -0,0 +1,43 @@
+package edu.yacoubi.softwaretesting.stripe;
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import com.stripe.net.RequestOptions;
+import edu.yacoubi.softwaretesting.payment.CardPaymentCharge;
+import edu.yacoubi.softwaretesting.payment.CardPaymentCharger;
+import edu.yacoubi.softwaretesting.payment.Currency;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+// implementation according to stripe api reference with some adjustment
+public class StripeService implements CardPaymentCharger {
+    private final RequestOptions requestOptions =
+            RequestOptions.builder()
+                    .setApiKey("sk_test_4eC39HqLyjWDarjtT1zdp7dc")
+                    .build();
+
+    @Override
+    public CardPaymentCharge chargeCard(
+            String cardSource,
+            BigDecimal amount,
+            Currency currency,
+            String description) {
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", amount);
+        params.put("currency", currency);
+        params.put("source", cardSource);
+        params.put("description", description);
+
+        try {
+            Charge charge = Charge.create(params, requestOptions);
+            return new CardPaymentCharge(charge.getPaid());
+        } catch (StripeException e) {
+            throw new IllegalStateException("cannot make stripe charge", e);
+        }
+    }
+}
