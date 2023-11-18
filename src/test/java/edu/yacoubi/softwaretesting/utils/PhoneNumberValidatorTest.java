d740147fcd95fdf0fb0fe3080b7c783a0a375f92
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-11-18 22:27:50 +0100

    TTD & parameterized test

---
+++ src/test/java/edu/yacoubi/softwaretesting/utils/PhoneNumberValidatorTest.java
@@ -0,0 +1,32 @@
+package edu.yacoubi.softwaretesting.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class PhoneNumberValidatorTest {
+    private PhoneNumberValidator underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new PhoneNumberValidator();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "+447000000000, true",
+            "+44700000000088878, false",
+            "447000000000, false"
+    })
+    @DisplayName("parameterized test : 3 different tests")
+    void itShouldValidatePhoneNumber(String phoneNumber, boolean expected) {
+        // When
+        boolean isValid = underTest.test(phoneNumber);
+
+        // Then
+        assertThat(isValid).isEqualTo(expected);
+    }
+}
